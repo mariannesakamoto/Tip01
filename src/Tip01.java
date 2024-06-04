8b9dc251427d1927553bfa971bf40d7b949dd381
ALUNO <ALUNO@10.85.198.7>
2024-06-04 16:29:08 -0300

    Commit project Tip01

---
+++ src/Tip01.java
@@ -0,0 +1,30 @@
+public class Tip01 {
+    public static void main(String[] args) {
+     double tax = 0.05; //valor da taxa
+double tip = 0.15;      //valor da gorjeta 
+double person1 = 10;
+double total1 = person1*(1 +tax +tip);   //valor da pessoa 1 + taxa e gorjeta
+System.out.println("person 1: " + total1);
+double person2 = 12;
+double total2 = person2*(1 +tax +tip);   //valor da pessoa 2 + taxa e gorjeta
+System.out.println("person 2: " + total2);
+double person3 = 9;
+double total3 = person3*(1 +tax +tip);   //valor da pessoa 3 + taxa e gorjeta
+System.out.println("person 3: " + total3);
+double person4 = 8;
+double total4 = person4*(1 +tax +tip);   //valor da pessoa 4 + taxa e gorjeta
+System.out.println("person 4: " + total4);
+double person5 = 17;
+double total5 = person5*(1 +tax +tip);   //valor da pessoa 5 + taxa e gorjeta
+System.out.println("person 5: " + total5);
+double person6 = 15;
+double total6 = person6*(1 +tax +tip);   //valor da pessoa 6 + taxa e gorjeta
+System.out.println("person 6: " + total6);
+double person7 = 11;
+double total7 = person7*(1 +tax +tip);   //valor da pessoa 7 + taxa e gorjeta
+System.out.println("person 7: " + total7);
+double person8 = 30;
+double total8 = person8*(1 +tax +tip);   //valor da pessoa 8 + taxa e gorjeta
+System.out.println("person 8: " + total8);
+    }
+}
